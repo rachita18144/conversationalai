d11ee3f48976224a287b8a0044d479df99000ab6
rachita18144 <rachita18144@iiitd.ac.in>
2025-02-15 11:06:54 +0530

    first commit

---
+++ src/components/ConnectedAnimation.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./ConnectedAnimation.css";
+
+function ConnectedAnimation() {
+  return (
+    <div className="ocean">
+      <div className="wave"></div>
+      <div className="wave"></div>
+    </div>
+  );
+}
+
+export default ConnectedAnimation;
